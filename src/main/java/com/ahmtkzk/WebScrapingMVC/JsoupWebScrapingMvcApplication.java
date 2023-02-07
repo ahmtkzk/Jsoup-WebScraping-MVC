f58a499c99c65b3ca213981b24120ef2c7249e35
ahmtkzk <ahmetkazak08@gmail.com>
2023-02-07 22:56:09 +0300

    Initial commit.

---
+++ src/main/java/com/ahmtkzk/WebScrapingMVC/JsoupWebScrapingMvcApplication.java
@@ -0,0 +1,13 @@
+package com.ahmtkzk.WebScrapingMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoupWebScrapingMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsoupWebScrapingMvcApplication.class, args);
+	}
+
+}
